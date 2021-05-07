1d372acdbfd54cd65959edff0fea5c4e1fcf64f0
LukeM1234 <luke@mallett.org>
2021-05-07 14:52:16 +0100

    Updating Companent.h

--- spice-simulator/Component.h
+++ spice-simulator/Component.h
@@ -1,13 +1,15 @@
 #pragma once
+#include "Node.h"
+#include <vector>
 
 struct Component {
-
+  std::vector<Node*> nodes;
 };
 
 struct CurrentSource : Component {
-
+  double current;
 };
 
 struct Resistor : Component {
-
+  double resistance;
 };
