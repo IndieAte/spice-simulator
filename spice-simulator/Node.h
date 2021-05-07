af62b4ef3be8435c8b62c982058a51ad0a8e2e62
Sebastian Gaume <45313375+IndieAte@users.noreply.github.com>
2021-05-07 14:50:07 +0100

    Update Node Structure
    Added fields to Node structure.

--- spice-simulator/Node.h
+++ spice-simulator/Node.h
@@ -1,5 +1,15 @@
 #pragma once
 
+#include <iostream>
+#include <vector>
+#include "Component.h"
+
+// Node Structure
+// Created for each circuit node, contains identifying information
+// and a vector of pointers to connected components
 struct Node {
-	
+	std::string name;
+	int number;
+
+	std::vector<Component*> components;
 };
