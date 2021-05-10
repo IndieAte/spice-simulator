3cfea7390fb95db730a1ecf604b4aa496498fab2
LukeM1234 <luke@mallett.org>
2021-05-10 14:07:39 +0100

    adding resistor

--- spice-simulator/Source.cpp
+++ spice-simulator/Source.cpp
@@ -4,8 +4,6 @@
 #include <vector>
 
 #include "Component.h"
-#include "Node.h"
-
 
 std::vector<std::string> string_split(const std::string& s) {
 	int c = 0;
@@ -22,39 +20,64 @@ std::vector<std::string> string_split(const std::string& s) {
 	}
 	return v;
 }
+int get_node_number(const std::string& s) {
+	if (s.length() > 1) {
+		return std::stoi(s.substr(1,4));
+	} else {
+		return 0;
+	}
+}
 
-Component* decode_line(std::string line) {
+Component decode_line(std::string line) {
 	std::vector<std::string> v = string_split(line);
 
+	// Component comp;
+
+
 	switch(line[0]) {
-		case 'R':
-			std::cout << "R" << std::endl;
+		case 'R': {
+			std::cout << "R" << get_node_number(v[1]) << get_node_number(v[2]) << std::endl;
+
+			Resistor r1(v[0], std::stod(v[3]), get_node_number(v[1]), get_node_number(v[2]));
+			return r1;
 			break;
-		case 'I':
+		}
+		case 'I': {
 			std::cout << "I" << std::endl;
+
+			Resistor r2(v[0], std::stod(v[3]), get_node_number(v[1]), get_node_number(v[2]));
+			return r2;
+
 			break;
-	}
+		}
+	}	
+
+	Resistor r3(v[0], std::stod(v[3]), get_node_number(v[1]), get_node_number(v[2]));
+	return r3;
+
 }
 
 
 int main() {
 
-	std::cout << "Test" << std::endl;
+	// std::cout << "Test" << std::endl;
 
 
-	std::ifstream infile;
-	infile.open("../testCircuit.cir");
-	std::vector<Component*> components;
+	// std::ifstream infile;
+	// infile.open("../testCircuit.cir");
+	// std::vector<Component> components;
  
-	if(!infile.is_open()){ 
-		return EXIT_FAILURE;
-	}
+	// if(!infile.is_open()){
+	// 	return EXIT_FAILURE;
+	// }
 
-	std::string tmp;
-	while(std::getline(infile,tmp)){
-		components.push_back(decode_line(tmp));
-	}
+	// std::string tmp;
+	// while(std::getline(infile,tmp)){
+	// 	components.push_back(decode_line(tmp));
+	// }
 
-	infile.close();
+	// infile.close();
 
+	Resistor r3("hi", 12, 1, 2);
+	r3.getConductance(1,2);
 }
